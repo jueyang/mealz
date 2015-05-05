952d39d2bd28274e292882c687c4d98e2d0544d4
Jue Yang <jueyang22@gmail.com>
2015-05-05 13:30:24 -0400

    add first ever test. ghetto!

---
+++ test.js
@@ -0,0 +1,7 @@
+var _ = require('lodash');
+var mealz = require('./mealz.js');
+
+var caroline = new mealz.Diner('Caroline',['wine','arugula salad','seared salmon']);
+var george = new mealz.Diner('George',['old fashion','lamb shoulder','coffee']);
+
+new mealz.Meal([caroline,george],0.2);
