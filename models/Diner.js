93775e2226d639024feec604a0489db9bd1192dd
Jue Yang <jueyang22@gmail.com>
2015-05-08 09:58:05 -0400

    config is used only in Diner model, can it be clearer on the global level?

--- models/Diner.js
+++ models/Diner.js
@@ -1,16 +1,17 @@
 var _ = require('lodash');
+var config = require('../config.json');
 
-function Diner(name, dishes, menu, taxRate){
+function Diner(name, dishes){
 
 	var consumed = [];
 
 	_.each(dishes,function(d){
-		var matching = _.find(menu,{'item':d});
+		var matching = _.find(config.menu,{'item':d});
 		consumed.push(_.result(matching,'price'));
 	});
 
 	var individualSum = _.sum(consumed),
-		tax = individualSum * taxRate;
+		tax = individualSum * config.taxRate;
 
 	this.dinerName = name;
 	this.total = individualSum + tax;
